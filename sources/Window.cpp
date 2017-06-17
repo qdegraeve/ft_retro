55fb00d92b11835fdf8d41d316d7942a2af104c4
Quentin DEGRAEVE <qdegraeve@gmail.com>
2017-06-17 19:09:04 +0200

    added window objects

---
+++ sources/Window.cpp
@@ -0,0 +1,74 @@
+#include "Window.hpp"
+#include <iostream>
+
+/*************************     CONSTRUCTORS     *******************************/
+
+Window::Window(void) {
+	return ;
+}
+
+Window::Window(int const lines, int const begin)
+	: _win(newwin(lines, COLS - 4, begin, 4)),
+	_lines(lines - 1),
+	_cols(COLS - 4 - 2) {
+	box(this->_win, 0, 0);
+	wrefresh(this->_win);
+	return ;
+}
+
+Window::Window(Window const & src) {
+
+	*this = src;
+
+	return ;
+}
+
+/*************************     DESTRUCTORS     ********************************/
+
+Window::~Window(void) {
+	std::cout << "Destructor called" << std::endl;
+}
+
+/*************************     OPERATORS OVERLOAD     *************************/
+
+Window &	Window::operator=(Window const & rhs) {
+
+	if (this != &rhs) {
+		this->_lines = rhs.get_lines();
+		this->_cols = rhs.get_cols();
+	}
+	
+	return *this;
+}
+
+/*************************     GETTERS      ***********************************/
+
+/*************************     SETTERS      ***********************************/
+
+/*************************     PUBLIC MEMBER FUNCTIONS      *******************/
+
+int			Window::get_lines(void) const {
+	return (this->_lines);
+}
+
+int			Window::get_cols(void) const {
+	return (this->_cols);
+}
+
+WINDOW		*Window::get_win(void) const {
+	return (this->_win);
+}
+
+/*************************     PRIVATE MEMBER FUNCTIONS     *******************/
+
+/*************************     NON MEMBER FUNTIONS     ************************/
+/*************************     EXTERNAL OVERLOADS     *************************/
+
+std::ostream &		operator<<(std::ostream & o, Window const & rhs) {
+	o << "nombres de lignes == " << rhs.get_lines()
+		<< std::endl << "nombre de colonnes == " << rhs.get_cols() << std::endl;
+
+	return (o);
+}
+
+
--- sources/main.cpp
+++ sources/main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include "Entity.hpp"
 #include "Game.hpp"
+#include "Window.hpp"
 
 int	max(int i1, int i2) {
 	if (i1 >= i2)
@@ -20,48 +21,55 @@ int	min(int i1, int i2) {
 
 int main()
 {
+	initscr();
+	cbreak();
+	noecho();
+	curs_set(0);
+	Window		menu(10, 2);
+	Window		playground(LINES - 13, 11);
 	Game		game(1);
 	Player&		mathilde = game.get_player(0);
 
 	mathilde.set_name("Mathilde");
-	int x = 0;
-	int y = 0;
+	mvwprintw(menu.get_win(), menu.get_lines() / 2, menu.get_cols() / 2 - 10, "BIENVENUE");
+	wrefresh(menu.get_win());
+	int x = 1;
+	int y = 1;
 	int c = 0;
 	char curs = 'O';
 	char *str;
 	str = (char*)malloc(0);
-	initscr();
-	keypad(stdscr, TRUE);
-	cbreak();
-	addch(curs);
-	printw("enter your name : ");
-	echo();
-	getstr(str);
-	printw("\nyour name is : % s", str);
-	nodelay(stdscr, TRUE);
+	keypad(playground.get_win(), TRUE);
+	nodelay(playground.get_win(), TRUE);
+	wrefresh(playground.get_win());
 	while (42) {
-		if ((c = getch()) != 27) {
-			mvaddch(y, x, ' ');
+		if ((c = wgetch(playground.get_win())) != 27) {
+			mvwaddch(playground.get_win(), y, x, ' ');
 			switch(c) {
 				case KEY_UP:
-					y = max(0, y - 1);
+					y = max(1, y - 1);
 					break;
 				case KEY_DOWN:
-					y = min(LINES, y + 1);
+					y = min(playground.get_lines(), y + 1);
 					break;
 				case KEY_RIGHT:
-					x = min(COLS, x + 1);
+					x = min(playground.get_cols(), x + 1);
 					break;
 				case KEY_LEFT:
-					x = max(0, x - 1);
+					x = max(1, x - 1);
 					break;
 			}
 			flushinp();
-			mvaddch(y, x, curs);
+			mvwaddch(playground.get_win(), y, x, curs);
 		}
 		if (c == 27)
-			break;
-		usleep(100000);
+			break ;
+		if (c == KEY_RESIZE) {
+			erase();
+			std::cout << "Resize is forbidden" << std::endl;
+			break ;
+		}
+		usleep(20000);
 	}
 	endwin();
 }
