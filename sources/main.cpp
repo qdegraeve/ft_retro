215de380cd1dbe364efad58aa3cfffab3f1f32bf
Mathilde RESSIER <ressier@stg-interactive.com>
2017-06-17 16:17:42 +0200

    main

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,18 +1,12 @@
 #include <iostream>
 #include "Entity.hpp"
-#include "Player.hpp"
-#include "Ennemy.hpp"
-#include "Bullet.hpp"
+#include "Game.hpp"
 
 int main()
 {
-	Player		mathilde("Mathilde");
+	Game		game(1);
+	Player&		mathilde = game.get_player(0);
 
-	std::cout << mathilde << std::endl;
-	for (int i = 0; i < LINES + 1; ++i)
-	{
-		mathilde.move(0, 1);
-		std::cout << mathilde << std::endl;
-	}
+	mathilde.set_name("Mathilde");
 	return (0);
 }
