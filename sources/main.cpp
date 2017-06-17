269c707e428b3e6d10ff439f4413af574bc27e87
Mathilde RESSIER <ressier@stg-interactive.com>
2017-06-17 15:14:55 +0200

    main

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include "Entity.hpp"
+#include "Player.hpp"
+#include "Ennemy.hpp"
+#include "Bullet.hpp"
 
 int main()
 {
-	Entity		toto("toto", 10, 1, 0, 0, 't', 1);
-
-	std::cout << toto << std::endl;
-	toto.move();
 	return (0);
 }
