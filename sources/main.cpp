6d6bc45c51717a1b2cc2fdf30b05c067e1dd155f
Mathilde RESSIER <ressier@stg-interactive.com>
2017-06-17 18:49:17 +0200

    clean main

--- sources/main.cpp
+++ sources/main.cpp
@@ -6,14 +6,8 @@ int main()
 {
 	Game		game(1);
 	Player&		mathilde = game.get_player(0);
-	Ennemy*		ennemy1;
-	Ennemy*		ennemy_list;
 
 	mathilde.set_name("Mathilde");
 	game.generate_ennemy();
-	ennemy_list = game.get_ennemy_list();
-	ennemy1 = (Ennemy *)Entity::get_one_entity(ennemy_list, 1);
-	std::cout << *ennemy1 << std::endl;
-	ennemy_list = (Ennemy *)Entity::delete_one_entity_on_list(ennemy_list, ennemy1);
 	return (0);
 }
