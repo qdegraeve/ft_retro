b245ca3f73401939174cda15888c0cff1be7200b
Mathilde RESSIER <ressier@stg-interactive.com>
2017-06-17 16:19:25 +0200

    Entity

--- includes/Entity.hpp
+++ includes/Entity.hpp
@@ -3,38 +3,67 @@
 
 #include <iostream>
 
+#ifndef LINES
+# define LINES 20
+#endif
+#ifndef COLONNES
+# define COLONNES 10
+#endif
+
 class Entity
 {
 	public:
+		/* constructors */
 		Entity(std::string type,
 				unsigned int life_max,
 				unsigned int damage_point,
 				unsigned int pos_x,
 				unsigned int pos_y,
+				unsigned char character,
 				int speed);								/* constructor by default */
 		Entity(Entity const & src);						/* constructor by copy */
+		/* destructor */
 		virtual ~Entity();								/* destructor */
 
-		Entity & operator=(Entity const & rhs);								/* overload operator = */
+		/* operator overload */
+		Entity & operator=(Entity const & rhs);
 
+		/* Getters */
+		std::string	const	get_type() const;
+		unsigned int		get_life() const;
+		unsigned int		get_life_max() const;
+		unsigned int		get_damage_point() const;
+		unsigned int		get_pos_x() const;
+		unsigned int		get_pos_y() const;
+		unsigned char		get_character() const;
+		int					get_speed() const;
 
-		void
+		/* Setters */
+		void				set_damage_point(unsigned int damage_point);
+		void				set_pos_x(unsigned int new_pos_x);
+		void				set_pos_y(unsigned int new_pos_y);
+		void				set_speed(int new_speed);
+		void				set_type(std::string const & new_type);
 
-		void	takeDamage(unsigned int amount) const;
-		void	move() const; //TEMP
+		/* Others */
+		void				take_damage(unsigned int amount);
+		void				move(int x_move, int y_move);
 
 	protected:
 
-			std::string	const		_type;				//to confirm
+			std::string				_type;
 			unsigned int			_life_max;
 			unsigned int			_life;
 			unsigned int			_damage_point;
 			unsigned int			_pos_x;
 			unsigned int			_pos_y;
+			unsigned char			_character;
 			int						_speed;
 
 	private:
 
+		unsigned int				_check_move(unsigned int current_pos, int move,
+										int pos_max, int pos_min) const;
 };
 
 std::ostream &		operator<<(std::ostream & o, Entity const & rhs);
